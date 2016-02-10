ee47cac8b4e036e34660fb97c487651fd3e5222f
Artem Vysotsky <artem.vysotsky@gmail.com>
2016-02-09 17:43:28 -0800

    added production config

---
+++ server/datasources.production.js
@@ -0,0 +1,25 @@
+// Use the same environment-based configuration as in staging
+var url = require('url');
+
+if (!process.env.MYSQL_URL) {
+  throw new Error('MYSQL_URL env variable not found');
+}
+var mysqlUrl = url.parse(process.env.MYSQL_URL);
+var auth     = mysqlUrl.auth.split(':');
+
+module.exports = {
+  "db":      {
+    "name":      "db",
+    "connector": "memory"
+  },
+  "mysqlDs": {
+    "host":      mysqlUrl.host,
+    "port":      mysqlUrl.port,
+    "url":       process.env.MYSQL_URL,
+    "database":  mysqlUrl.pathname,
+    "password":  auth[1],
+    "name":      "mysqlDs",
+    "user":      auth[0],
+    "connector": "mysql"
+  }
+}
