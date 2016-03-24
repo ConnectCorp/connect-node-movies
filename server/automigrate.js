1e19474fb1e92ae57ea7aa314e39abea7594b748
Artem Vysotsky <artem.vysotsky@gmail.com>
2016-03-23 19:16:28 -0700

    [ENG-86] new connect-node-middleware and rockerfile

--- server/automigrate.js
+++ server/automigrate.js
@@ -5,7 +5,7 @@ var modelConfig = require('./model-config.json');
 
 var tables = Object.keys(modelConfig);
 
-ds.automigrate(tables, function (er) {
+ds.autoupdate(tables, function (er) {
   if (er) throw er;
   console.log('Loopback tables [' + tables + '] created in ', ds.adapter.name);
   ds.disconnect();
--- server/boot/001-connect-jwt.js
+++ server/boot/001-connect-jwt.js
@@ -1,43 +1,68 @@
-var jwt      = require('express-jwt');
+var jwt = require('express-jwt');
+var async = require('async');
+var Transaction = require('loopback-connector').Transaction;
+var connectMiddleware = require('connect-node-middleware');
 
 module.exports = function (app) {
 
-  var secret = process.env.CONNECT_JWT_SECRET;
+  var base64secret = process.env.CONNECT_JWT_SECRET;
+  var secret = base64secret && new Buffer(base64secret, 'base64');
+  var User = app.models.Person;
 
   if (!secret) {
     return;
   }
 
-  app.use('/api', jwt({
-    secret:          secret.replace(/\\n/g, "\n"),
-    requestProperty: 'jwt'
+  function login(email, next) {
+    User.login({
+      email: email,
+      password: secret
+    }, next);
+  }
+
+  app.use('/api', connectMiddleware({
+    secret: secret,
+    baseUri: process.env.CONNECT_AUTH_BASE_URL
   }));
 
   app.use('/api', function (req, res, next) {
+    var tx, u;
 
-    var User = app.models.Person;
+    async.waterfall([
+      function (next) {
+        User.beginTransaction(Transaction.READ_COMMITTED, next);
+      },
+      function (t, next) {
+        tx = t;
+        req.user.data.id = req.user.data;
+        User.findOne({where: {username: req.user.userId}}, {transaction: tx}, next);
+      },
+      function (user, next) {
+        if (user) {
+          return next(null, user);
+        }
 
-    var email = req.jwt.email || req.jwt.sub + '@movies.connect.com';
+        var email = req.user.emailAccounts && req.user.emailAccounts.lengh > 0 && req.user.emailAccounts[0].address;
+        email = email || req.user.userId + '@movies.connect.com';
 
-    var data = {
-      username:      "" + req.jwt.sub,
-      email:         email,
-      pictureUrl:    req.jwt.picture || '',
-      name:          req.jwt.name || email,
-      phoneNumber:   req.jwt.phone || '',
-      emailVerified: true,
-      password:      secret
-    };
+        var data = {
+          username: req.user.userId,
+          email: email,
+          pictureUrl: req.user.userId.pictureUrl,
+          name: req.user.userId.name,
+          emailVerified: true,
+          password: base64secret
+        };
 
-    User.findOrCreate({where: {username: data.username}}, data, function (err, user) {
-      if (err) {
-        return next(err);
+        User.create(data, {transaction: tx}, next);
+      },
+      function (user, next) {
+        u = user;
+        tx.commit(next);
+      },
+      function (next) {
+        return login(u.email, next);
       }
-
-      User.login({
-        email:    user.email,
-        password: secret
-      }, next)
-    });
+    ], next);
   });
 };
